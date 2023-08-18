5a2cf36c42882b0504c11a02731f0c38d2af6b4b
Gabriel Fleury <gabrielfleury95@gmail.com>
2023-08-18 13:52:50 -0300

    firebase

---
+++ src/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAP2JFtRnsSs5teUATlcua_f-CGoGPoFyk",
+  authDomain: "python-be-test.firebaseapp.com",
+  projectId: "python-be-test",
+  storageBucket: "python-be-test.appspot.com",
+  messagingSenderId: "81168893574",
+  appId: "1:81168893574:web:b0f5af64e2cc77ba367307"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
